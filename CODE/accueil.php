2d3e9c1cd7df37be67769af723af3de317bbc4a6
KikosS41 <killian.legendre41@gmail.com>
2021-03-19 14:51:59 +0100

    ajout script

--- CODE/accueil.php
+++ CODE/accueil.php
@@ -1,3 +1,28 @@
+<script>
+        window.onscroll = function() {scrollFunction()};
+
+        function scrollFunction() {
+        if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+            document.getElementById("navbar").style.padding = "5px 10px";
+        } else {
+            document.getElementById("navbar").style.padding = "40px 10px";
+        }
+        }
+
+        var conButton = document.getElementById("connexionButton");
+        var conPage = document.getElementById("connexionPage");
+        function afficherConPage(){
+        if(conPage.style.display=="none")
+        {
+            conPage.style.display="block";
+        }
+        else{
+            conPage.style.display="none";
+        }
+        }
+        conButton.addEventListener('click', function(){afficherConPage()});
+    </script>
+
 <?php
 include_once('./assets/libs/Smarty.class.php');
 
--- CODE/templates/header.tpl
+++ CODE/templates/header.tpl
@@ -21,28 +21,3 @@
         </div>
 
     </header>
-
-    <script>
-        window.onscroll = function() {scrollFunction()};
-
-        function scrollFunction() {
-        if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
-            document.getElementById("navbar").style.padding = "5px 10px";
-        } else {
-            document.getElementById("navbar").style.padding = "40px 10px";
-        }
-        }
-
-        var conButton = document.getElementById("connexionButton");
-        var conPage = document.getElementById("connexionPage");
-        function afficherConPage(){
-        if(conPage.style.display=="none")
-        {
-            conPage.style.display="block";
-        }
-        else{
-            conPage.style.display="none";
-        }
-        }
-        conButton.addEventListener('click', function(){afficherConPage()});
-    </script>
