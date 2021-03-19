bb1a573ed4dfe4f34b9852778d76f88aca0300e5
KikosS41 <killian.legendre41@gmail.com>
2021-03-19 15:45:23 +0100

    creation accueil.php

--- CODE/accueil.php
+++ CODE/accueil.php
@@ -1,47 +1,27 @@
+<?php
+    include_once('./assets/libs/Smarty.class.php');
+    $obj = new smarty; 
+?>
+
 <!doctype html>
     <html lang="fr">
 
-    <head>
-        <meta charset="utf-8">
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">        
-        <link rel="stylesheet" href="./assets/vendors/fontawesome-free-5.15.1-web/css/all.min.css">
-        <link rel="stylesheet" href="./assets/css/style.css">
-        <title>LeBonStage</title>
-    </head>
+        <?php
+            $obj->assign()
+            $obj->display('./tpl/head.tpl');
+        ?>
 
-<script>
-        window.onscroll = function() {scrollFunction()};
 
-        function scrollFunction() {
-        if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
-            document.getElementById("navbar").style.padding = "5px 10px";
-        } else {
-            document.getElementById("navbar").style.padding = "40px 10px";
-        }
-        }
+        <body>
 
-        var conButton = document.getElementById("connexionButton");
-        var conPage = document.getElementById("connexionPage");
-        function afficherConPage(){
-        if(conPage.style.display=="none")
-        {
-            conPage.style.display="block";
-        }
-        else{
-            conPage.style.display="none";
-        }
-        }
-        conButton.addEventListener('click', function(){afficherConPage()});
-    </script>
+            <?php  
+                $obj->display('./tpl/header.tpl');
+                $obj->display('./tpl/footer.tpl');
+            ?>
 
-<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous"></script>
-<script src="./assets/js/code.index.js"></script>
+            <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+            <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous"></script>
+            <script src="./assets/js/code.index.js"></script>
 
-<?php
-    include_once('./assets/libs/Smarty.class.php');
-
-    $obj = new smarty; 
-    $obj->display('./tpl/header.tpl');
-    $obj->display('./tpl/footer.tpl');
-?>
+        </body>
+    </html>
---
+++ CODE/templates_c/9d3ec62dc02615be382d356ff91756231a1b16a6_0.file.footer.tpl.php
@@ -0,0 +1,76 @@
+<?php
+/* Smarty version 3.1.39, created on 2021-03-19 15:24:15
+  from 'C:\Users\killi\Desktop\A2\4- developpement Web\Projet_web\CODE\tpl\footer.tpl' */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '3.1.39',
+  'unifunc' => 'content_6054b40fd0bce3_88800430',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    '9d3ec62dc02615be382d356ff91756231a1b16a6' => 
+    array (
+      0 => 'C:\\Users\\killi\\Desktop\\A2\\4- developpement Web\\Projet_web\\CODE\\tpl\\footer.tpl',
+      1 => 1616160742,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+  ),
+),false)) {
+function content_6054b40fd0bce3_88800430 (Smarty_Internal_Template $_smarty_tpl) {
+?>    <footer class="row justify-content-around brd">
+      <!-- reseaux sociaux -->
+      <div class="col align-self-center">
+        <div class="container">
+          <div class="row justify-content-center">
+            <div class="col-auto"><h2>Retrouvez nous sur</h2></div>
+          </div>
+          <div class="row justify-content-around">
+            <div class="col-auto"><i id="facebook" class="fab fa-facebook-square"></i></div>
+            <div class="col-auto"><i id="linkedin" class="fab fa-linkedin"></i></div>
+            <div class="col-auto"><i id="instagram" class="fab fa-instagram-square"></i></div>
+          </div>
+        </div>
+      </div>
+      <!-- Postes des chefs -->
+      <div class="col align-self-center">
+        <div class="container">
+          <div class="row justify-content-center">
+            <div class="col-auto"><h2>Qui sommes-nous ?</h2></div>
+          </div>
+          <div class="row justify-content-center">
+            <div class="col-auto">CEO : Theo Delgeon</div>
+          </div>
+          <div class="row justify-content-center">
+            <div class="col-auto">Marketing Director : Killian Legendre</div>
+          </div>
+          <div class="row justify-content-center">
+            <div class="col-auto">Community Manager : Armand Debesse</div>
+          </div>
+        </div>
+      </div>
+          <!-- Contact -->
+      <div class="col align-self-center">
+        <div class="container">
+          <div class="row justify-content-center">
+            <div class="col-auto"><h2>Contact</h2></div>
+          </div>
+          <div class="row justify-content-center">
+            <div class="col-auto"><i class="fas fa-envelope"></i></div>
+            <div class="col-auto align-self-center">contact@lebonstage.com</div>
+          </div>
+          <div class="row justify-content-center">
+            <div class="col-auto"><i class="fas fa-phone-square-alt"></i></div>
+            <div class="col-auto align-self-center">02 36 45 91 22</div>
+          </div>
+          <div class="row justify-content-center">
+            <div class="col-auto"><i class="fas fa-home"></i></div>
+            <div class="col-auto align-self-center">10 av du stage 45160 Olivet</div>
+          </div>
+        </div>
+      </div>
+    </footer><?php }
+}
