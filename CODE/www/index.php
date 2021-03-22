be783b9fd603f712d06a2070af0a081bdef05af7
KikosS41 <killian.legendre41@gmail.com>
2021-03-22 12:13:18 +0100

    modif du formulaire

--- CODE/www/index.php
+++ CODE/www/index.php
@@ -4,7 +4,18 @@
     if (isset($_GET['action'])) {
         switch ($_GET['action']){
             case 'PDView':
-                listPD($_GET['nom_del'], $_GET['prenom_del'], $_GET['nom_pil'], $_GET['prenom_pil']);
+                if(isset($_GET['nom_del']) && isset($_GET['prenom_del']) && !isset($_GET['nom_pil']) && !isset($_GET['prenom_pil']))
+                {
+                    listPD($_GET['nom_del'], $_GET['prenom_del'], null,null);
+                }
+                else if(!isset($_GET['nom_del']) && !isset($_GET['prenom_del']) && isset($_GET['nom_pil']) && isset($_GET['prenom_pil']))
+                {
+                    listPD(null, null, $_GET['nom_pil'], $_GET['prenom_pil']);
+                }
+                else
+                {
+                    listPD(null, null, null, null);
+                }
                 break;
         }
     }
