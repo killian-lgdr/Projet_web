9bf30976082c2c3d27c122fc9e68637a8294164a
KikosS41 <killian.legendre41@gmail.com>
2021-03-22 09:37:48 +0100

    ajout contrl del pil

---
+++ CODE/www/view/templates_c/5b015ca1d5827112e54fdce82e89841fe4ecb356_0.file.script.tpl.php
@@ -0,0 +1,33 @@
+<?php
+/* Smarty version 3.1.39, created on 2021-03-22 09:06:59
+  from 'C:\Users\killi\Desktop\A2\4- developpement Web\Projet_web\CODE\www\public\tpl\script.tpl' */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '3.1.39',
+  'unifunc' => 'content_60585023625c18_19642805',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    '5b015ca1d5827112e54fdce82e89841fe4ecb356' => 
+    array (
+      0 => 'C:\\Users\\killi\\Desktop\\A2\\4- developpement Web\\Projet_web\\CODE\\www\\public\\tpl\\script.tpl',
+      1 => 1616168450,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+  ),
+),false)) {
+function content_60585023625c18_19642805 (Smarty_Internal_Template $_smarty_tpl) {
+?>            <?php echo '<script'; ?>
+ src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"><?php echo '</script'; ?>
+>
+            <?php echo '<script'; ?>
+ src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous"><?php echo '</script'; ?>
+>
+            <?php echo '<script'; ?>
+ src="../public/js/code.index.js"><?php echo '</script'; ?>
+><?php }
+}
