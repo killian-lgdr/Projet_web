e93d6eccd5e9015a278c7268f09f47faeb0331a4
KikosS41 <killian.legendre41@gmail.com>
2021-03-24 12:39:31 +0100

    correction requete addpilote()

--- CODE/www/model/PDManager.php
+++ CODE/www/model/PDManager.php
@@ -29,10 +29,14 @@
         {
 
             $db = $this->dbConnect();
-                $req = $db->prepare('Insert into localisation (nom_Localisation) Select :ville where not exists(SELECT nom from localisation where nom_Localisation = :ville ) ;
-                Insert into identifiants (nom_Identifiant, mdp_Identifiant) Select :identifiant , :mdp Where not exists(select * from identifiants where nom_Identifiant = :identifiant AND mdp_Identifiant = :mdp );
-                IF NOT EXIST INSERT INTO `pilote`(`nom_Pilote`, `prenom_Pilote`, `ID_Localisation`, `ID_Identifiant`) VALUES (:nom , :prenom , (SELECT ID_localisation FROM localisation Where localisation.nom_localisation = :ville ) , (SELECT ID_Identifiant FROM identifiants Where identifiants.nom_Identifiant = :identifiant ));
-                IF NOT EXIST INSERT INTO `enseigne_a`(`ID_Pilote`, `ID_NiveauEtudes`) VALUES ((SELECT ID_Pilote FROM pilote Where pilote.nom_Pilote = :nom AND pilote.prenom_Pilote = :prenom ), (SELECT ID_NiveauEtudes FROM niveauetudes Where niveauetudes.promotion= :promotion ))
+                $req = $db->prepare('
+                Insert into localisation (nom_Localisation) Select :ville where not exists(SELECT nom_Localisation from localisation where nom_Localisation = :ville ) ;
+
+                Insert into identifiants (nom_Identifiant, mdp_Identifiant) Select :identifiant , :mdp Where not exists(select nom_Identifiant, mdp_Identifiant from identifiants where nom_Identifiant = :identifiant AND mdp_Identifiant = :mdp );
+
+                IF NOT EXIST INSERT INTO `pilote`(`nom_Pilote`, `prenom_Pilote`, `ID_Localisation`, `ID_Identifiant`) VALUES (:nom , :prenom , (SELECT ID_Localisation FROM localisation Where localisation.nom_Localisation = :ville ) , (SELECT ID_Identifiant FROM identifiants Where identifiants.nom_Identifiant = :identifiant ) );
+
+                IF NOT EXIST INSERT INTO `enseigne_a`(`ID_Pilote`, `ID_NiveauEtudes`) VALUES ((SELECT ID_Pilote FROM pilote Where pilote.nom_Pilote = :nom AND pilote.prenom_Pilote = :prenom ),(SELECT ID_NiveauEtudes FROM niveauetudes Where niveauetudes.promotion= :promotion ));
                 ');
 
                 for ($i=0; $i < promotion.lenght(); $i+=2) { 
