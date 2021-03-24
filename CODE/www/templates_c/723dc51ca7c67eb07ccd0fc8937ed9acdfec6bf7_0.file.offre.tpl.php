d7e37b23258d38fafe8b5b2349f68707279560f1
SenseiDeleo <theodelgeon@gmail.com>
2021-03-24 10:36:57 +0100

    ajout requete

--- CODE/www/templates_c/723dc51ca7c67eb07ccd0fc8937ed9acdfec6bf7_0.file.offre.tpl.php
+++ CODE/www/templates_c/723dc51ca7c67eb07ccd0fc8937ed9acdfec6bf7_0.file.offre.tpl.php
@@ -1,18 +1,18 @@
 <?php
-/* Smarty version 3.1.39, created on 2021-03-23 10:28:50
+/* Smarty version 3.1.39, created on 2021-03-24 10:08:05
   from 'C:\Users\33610\Desktop\CESI\Cours CESI\A2\4. Web\Projet\Projet Git\Projet_web\CODE\www\public\tpl\offre.tpl' */
 
 /* @var Smarty_Internal_Template $_smarty_tpl */
 if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   'version' => '3.1.39',
-  'unifunc' => 'content_6059b4d2e0c7f6_02647281',
+  'unifunc' => 'content_605b017567cf19_13341932',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
     '723dc51ca7c67eb07ccd0fc8937ed9acdfec6bf7' => 
     array (
       0 => 'C:\\Users\\33610\\Desktop\\CESI\\Cours CESI\\A2\\4. Web\\Projet\\Projet Git\\Projet_web\\CODE\\www\\public\\tpl\\offre.tpl',
-      1 => 1616402546,
+      1 => 1616576879,
       2 => 'file',
     ),
   ),
@@ -20,7 +20,7 @@ if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   array (
   ),
 ),false)) {
-function content_6059b4d2e0c7f6_02647281 (Smarty_Internal_Template $_smarty_tpl) {
+function content_605b017567cf19_13341932 (Smarty_Internal_Template $_smarty_tpl) {
 ?>                        <div class="row littleMarge" id="division<?php echo $_smarty_tpl->tpl_vars['id']->value;?>
 ">
                         <div class="col-12 container brd blue">
@@ -38,12 +38,12 @@ function content_6059b4d2e0c7f6_02647281 (Smarty_Internal_Template $_smarty_tpl)
 </p>
                                 </div>
                                 <div class="col">
-                                    <p>Niveau Etudes min : <?php echo $_smarty_tpl->tpl_vars['nivetudes']->value;?>
+                                    <p>Niveau Etudes min : A<?php echo $_smarty_tpl->tpl_vars['nivetudes']->value;?>
 </p>
                                 </div>
                                 <div class="col">
                                     <p>Duree : <?php echo $_smarty_tpl->tpl_vars['duree']->value;?>
-</p>
+ mois</p>
                                 </div>
                                 <div class="col">
                                     <p>Date de Debut : <?php echo $_smarty_tpl->tpl_vars['date']->value;?>
@@ -55,7 +55,7 @@ function content_6059b4d2e0c7f6_02647281 (Smarty_Internal_Template $_smarty_tpl)
                                 </div>
                                 <div class="col">
                                     <p>Salaire : <?php echo $_smarty_tpl->tpl_vars['salaire']->value;?>
-</p>
+ €</p>
                                 </div>
                             </div>
                             <div class="row justify-content-center" id="buttonsHover<?php echo $_smarty_tpl->tpl_vars['id']->value;?>
@@ -64,6 +64,7 @@ function content_6059b4d2e0c7f6_02647281 (Smarty_Internal_Template $_smarty_tpl)
                                 <div class="col-auto"><button>J'ai postulé</button></div>
                             </div>
                         </div>
+                        
                     </div>
                     <?php echo '<script'; ?>
 >
