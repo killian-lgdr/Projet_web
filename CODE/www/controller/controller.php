018f7d8f518255119a06b495b24fe88d0f8bed4c
KikosS41 <killian.legendre41@gmail.com>
2021-03-24 17:08:10 +0100

    creer pilote

--- CODE/www/controller/controller.php
+++ CODE/www/controller/controller.php
@@ -68,7 +68,6 @@
             $ville = $_POST['ville_pil'];
             $identifiant =  $_POST['nom_pil'] . "." . $_POST['prenom_pil'];
             $mdp = password_hash($_POST['mdp_pil'], PASSWORD_DEFAULT);
-            //$mdp = $_POST['mdp_pil'];
 
             $i = 0;
             if (isset($_POST['promotion_pil'])){
