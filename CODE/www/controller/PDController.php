80f33c06b2101b956d12d0896374e013dbbac3a0
KikosS41 <killian.legendre41@gmail.com>
2021-03-26 17:05:47 +0100

    ajout attribution droit recherche

--- CODE/www/controller/PDController.php
+++ CODE/www/controller/PDController.php
@@ -5,12 +5,14 @@ require_once('./model/PDManager.php');
 function listPD(){
         
     $PDManager = new PDManager();
-
+//===============Délegué===============
+//rechercher
     if (isset($_POST['rechercher_del'])) {
         $nom = $_POST['nom_del'];
         $prenom = $_POST['prenom_del'];
         $delegue = $PDManager->getDelegue($nom, $prenom);
     }
+//creer
     if (isset($_POST['creer_del']) && isset($_POST['nom_del']) && isset($_POST['prenom_del']) && isset($_POST['ville_del']) && isset($_POST['mdp_del'])&& isset($_POST['confmdp_del']) && $_POST['mdp_del'] == $_POST['confmdp_del']) {
         $nom = $_POST['nom_del'];
         $prenom = $_POST['prenom_del'];
@@ -34,28 +36,21 @@ function listPD(){
         echo $droit;
         $delegue = $PDManager->addDelegue($nom, $prenom, $ville, $identifiant, $mdp, $droit);
     }
+//suppression
     if (isset($_POST['supprimer_del'])&& isset($_POST['nom_del']) && isset($_POST['prenom_del'])) {
         $nom = $_POST['nom_del'];
         $prenom = $_POST['prenom_del'];
         $identifiant =  $_POST['nom_del'] . "." . $_POST['prenom_del'];
         $pilote = $PDManager->deleteDelegue($nom, $prenom, $identifiant);
     }
-
-
-
-
-
-
-
-
-
-
+//===============Pïlote===============
+//rechercher
     if (isset($_POST['rechercher_pil'])) {
         $nom = $_POST['nom_pil'];
         $prenom = $_POST['prenom_pil'];
         $pilote = $PDManager->getPilote($nom, $prenom);
     }
-    
+//creer
     if (isset($_POST['creer_pil'])&& isset($_POST['nom_pil']) && isset($_POST['prenom_pil']) && isset($_POST['ville_pil']) && isset($_POST['mdp_pil'])&& isset($_POST['confmdp_pil']) && $_POST['mdp_pil'] == $_POST['confmdp_pil']) {
         $nom = $_POST['nom_pil'];
         $prenom = $_POST['prenom_pil'];
@@ -76,27 +71,28 @@ function listPD(){
             }
         }
     }
-
-        if (isset($_POST['supprimer_pil'])&& isset($_POST['nom_pil']) && isset($_POST['prenom_pil'])) {
-            $nom = $_POST['nom_pil'];
-            $prenom = $_POST['prenom_pil'];
-            $identifiant =  $_POST['nom_pil'] . "." . $_POST['prenom_pil'];
-            $pilote = $PDManager->deletePilote($nom, $prenom, $identifiant);
-        }
-    require_once('./view/PDview.php');
-
-    function CompareAucun($debut, $fin, $var)
+//suppression
+    if (isset($_POST['supprimer_pil'])&& isset($_POST['nom_pil']) && isset($_POST['prenom_pil'])) {
+        $nom = $_POST['nom_pil'];
+        $prenom = $_POST['prenom_pil'];
+        $identifiant =  $_POST['nom_pil'] . "." . $_POST['prenom_pil'];
+        $pilote = $PDManager->deletePilote($nom, $prenom, $identifiant);
+    }
+//recherche des droits
+    function CompareAucun($test, $var)
     {
         if (isset($_POST['rechercher_del'])) {
-            for ($i=$debut; $i<=$fin  ; $i++) { 
-                if(substr_count($var['ges_droit'],$i) !=0){
-                    return "";
-                    break;
-                }
-                if ($i == $fin && substr_count($var['ges_droit'],$i) ==0) {
+            for ($i=0; $i < strlen($var['ges_droit'])-1; $i+=3) { 
+                if ($listdroit[$i].$listdroit[$i+1]== $test){
                     return "selected";
                 }
             }
+            return "";
+        }
+        else{
+            return 'selected';
         }
     }
+
+    require_once('./view/PDview.php');
 }
--- CODE/www/model/PDManager.php
+++ CODE/www/model/PDManager.php
@@ -59,39 +59,38 @@
         {
 
             $db = $this->dbConnect();
-                $req1 = $db->prepare("Insert into localisation (nom_Localisation) Select :ville1 where not exists(SELECT nom_Localisation from localisation where nom_Localisation = :ville2 ) ;");
-                $req1->execute(array('ville1' => $ville, 'ville2' => $ville));
+
+            $req1 = $db->prepare("Insert into localisation (nom_Localisation) Select :ville1 where not exists(SELECT nom_Localisation from localisation where nom_Localisation = :ville2 ) ;");
+            $req1->execute(array('ville1' => $ville, 'ville2' => $ville));
                 
-                $req2 = $db->prepare("Insert into identifiants (nom_Identifiant, mdp_Identifiant) Select :identifiant1 , :mdp1 Where not exists(select nom_Identifiant, mdp_Identifiant from identifiants where nom_Identifiant = :identifiant2 );");
-                $req2->execute(array('identifiant1' => $identifiant, 'mdp1' => $mdp, 'identifiant2' => $identifiant));
+            $req2 = $db->prepare("Insert into identifiants (nom_Identifiant, mdp_Identifiant) Select :identifiant1 , :mdp1 Where not exists(select nom_Identifiant, mdp_Identifiant from identifiants where nom_Identifiant = :identifiant2 );");
+            $req2->execute(array('identifiant1' => $identifiant, 'mdp1' => $mdp, 'identifiant2' => $identifiant));
                 
-                $req3 = $db->prepare("INSERT INTO pilote (nom_Pilote, prenom_Pilote, ID_Localisation, ID_Identifiant) SELECT :nom1 , :prenom1 ,(SELECT ID_Localisation FROM localisation WHERE localisation.nom_Localisation = :ville1 ),(SELECT ID_Identifiant FROM identifiants WHERE identifiants.nom_Identifiant = :identifiant1 AND identifiants.mdp_Identifiant = :mdp1 ) WHERE NOT EXISTS (SELECT nom_Pilote, prenom_Pilote FROM pilote WHERE pilote.nom_Pilote = :nom2 AND pilote.prenom_Pilote = :prenom2 );");
-                $req3->execute(array('nom1' => $nom, 'prenom1' => $prenom, 'ville1' => $ville, 'identifiant1' => $identifiant, 'mdp1' => $mdp, 'nom2' => $nom, 'prenom2' => $prenom));
+            $req3 = $db->prepare("INSERT INTO pilote (nom_Pilote, prenom_Pilote, ID_Localisation, ID_Identifiant) SELECT :nom1 , :prenom1 ,(SELECT ID_Localisation FROM localisation WHERE localisation.nom_Localisation = :ville1 ),(SELECT ID_Identifiant FROM identifiants WHERE identifiants.nom_Identifiant = :identifiant1 AND identifiants.mdp_Identifiant = :mdp1 ) WHERE NOT EXISTS (SELECT nom_Pilote, prenom_Pilote FROM pilote WHERE pilote.nom_Pilote = :nom2 AND pilote.prenom_Pilote = :prenom2 );");
+            $req3->execute(array('nom1' => $nom, 'prenom1' => $prenom, 'ville1' => $ville, 'identifiant1' => $identifiant, 'mdp1' => $mdp, 'nom2' => $nom, 'prenom2' => $prenom));
                 
 
-                $req4 = $db->prepare("INSERT INTO enseigne_a (ID_Pilote, ID_NiveauEtudes) VALUES ((SELECT ID_Pilote FROM pilote Where pilote.nom_Pilote = :nom1 AND pilote.prenom_Pilote = :prenom1 ),(SELECT ID_NiveauEtudes FROM niveauetudes Where niveauetudes.promotion= :promotion1 ));");
-                for ($i=0; $i < strlen($promotion)-1; $i+=2) { 
-                    $req4->execute(array('nom1' => $nom, 'prenom1' => $prenom, 'promotion1' => ($promotion[$i].$promotion[$i+1])));
-                }
+            $req4 = $db->prepare("INSERT INTO enseigne_a (ID_Pilote, ID_NiveauEtudes) VALUES ((SELECT ID_Pilote FROM pilote Where pilote.nom_Pilote = :nom1 AND pilote.prenom_Pilote = :prenom1 ),(SELECT ID_NiveauEtudes FROM niveauetudes Where niveauetudes.promotion= :promotion1 ));");
+            for ($i=0; $i < strlen($promotion)-1; $i+=2) { 
+                $req4->execute(array('nom1' => $nom, 'prenom1' => $prenom, 'promotion1' => ($promotion[$i].$promotion[$i+1])));
+            }
         }
 
         public function deletePilote($nom, $prenom, $identifiant)
         {
             $db = $this->dbConnect();
 
-                $req1 = $db->prepare("DELETE FROM enseigne_a WHERE ID_Pilote = (SELECT pilote.ID_Pilote FROM pilote WHERE pilote.nom_Pilote = :nom1 AND pilote.prenom_Pilote = :prenom1 );");
-                $req1->execute(array('nom1' => $nom, 'prenom1' => $prenom));
-            
-                $req2 = $db->prepare("DELETE FROM possedenotepilote WHERE ID_Pilote = (SELECT pilote.ID_Pilote FROM pilote WHERE pilote.nom_Pilote = :nom1 AND pilote.prenom_Pilote = :prenom1);");
-                $req2->execute(array('nom1' => $nom, 'prenom1' => $prenom));
+            $req1 = $db->prepare("DELETE FROM enseigne_a WHERE ID_Pilote = (SELECT pilote.ID_Pilote FROM pilote WHERE pilote.nom_Pilote = :nom1 AND pilote.prenom_Pilote = :prenom1 );");
+            $req1->execute(array('nom1' => $nom, 'prenom1' => $prenom));
+        
+            $req2 = $db->prepare("DELETE FROM possedenotepilote WHERE ID_Pilote = (SELECT pilote.ID_Pilote FROM pilote WHERE pilote.nom_Pilote = :nom1 AND pilote.prenom_Pilote = :prenom1);");
+            $req2->execute(array('nom1' => $nom, 'prenom1' => $prenom));
 
-                $req3= $db->prepare("DELETE FROM pilote WHERE pilote.nom_Pilote = :nom1 AND pilote.prenom_Pilote = :prenom1 ");
-                $req3->execute(array('nom1' => $nom, 'prenom1' => $prenom));
+            $req3= $db->prepare("DELETE FROM pilote WHERE pilote.nom_Pilote = :nom1 AND pilote.prenom_Pilote = :prenom1 ");
+            $req3->execute(array('nom1' => $nom, 'prenom1' => $prenom));
 
-                $req4 = $db->prepare("DELETE FROM identifiants WHERE nom_Identifiant= :identifiant1; ");
-                $req4->execute(array(':identifiant1' => $identifiant));
-
-                return $req1;
+            $req4 = $db->prepare("DELETE FROM identifiants WHERE nom_Identifiant= :identifiant1; ");
+            $req4->execute(array(':identifiant1' => $identifiant));
         }
     }
 ?>
