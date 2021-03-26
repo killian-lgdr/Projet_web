0769c60af66236e47c6be3702a570d66d2f15fa3
KikosS41 <killian.legendre41@gmail.com>
2021-03-26 15:18:21 +0100

    ajout delegue

--- CODE/www/model/PDManager.php
+++ CODE/www/model/PDManager.php
@@ -20,8 +20,26 @@
         public function addDelegue($nom, $prenom, $ville, $identifiant, $mdp, $droit)
         {
             $db = $this->dbConnect();
-                $req = $db->prepare('');
-                $req->execute(array('nom' => $nom, 'prenom' => $prenom, 'ville' => $ville,'identifiant' => $identifiant, 'mdp' => $mdp));
+            $req1 = $db->prepare("Insert into localisation (nom_Localisation) Select :ville1 where not exists(SELECT nom_Localisation from localisation where nom_Localisation = :ville2 ) ;");
+            $req1->execute(array('ville1' => $ville, 'ville2' => $ville));
+            
+            $req2 = $db->prepare("Insert into identifiants (nom_Identifiant, mdp_Identifiant) Select :identifiant1 , :mdp1 Where not exists(select nom_Identifiant, mdp_Identifiant from identifiants where nom_Identifiant = :identifiant2 );");
+            $req2->execute(array('identifiant1' => $identifiant, 'mdp1' => $mdp, 'identifiant2' => $identifiant));
+
+            $req3 = $db->prepare("INSERT INTO delegue (nom_Delegue, prenom_Delegue, ID_Localisation, ID_Identifiant) SELECT :nom1 , :prenom1 ,(SELECT ID_Localisation FROM localisation WHERE localisation.nom_Localisation = :ville1 ),(SELECT ID_Identifiant FROM identifiants WHERE identifiants.nom_Identifiant = :identifiant1 AND identifiants.mdp_Identifiant = :mdp1 ) WHERE NOT EXISTS (SELECT nom_Delegue, prenom_Delegue FROM delegue WHERE delegue.nom_Delegue = :nom2 AND delegue.prenom_Delegue = :prenom2 );");
+            $req3->execute(array('nom1' => $nom, 'prenom1' => $prenom, 'ville1' => $ville, 'identifiant1' => $identifiant, 'mdp1' => $mdp, 'nom2' => $nom, 'prenom2' => $prenom));
+
+            $req4 = $db->prepare("INSERT INTO attribue (ID_Identifiant, ID_droits) VALUES ((SELECT ID_Identifiant FROM identifiants Where identifiants.nom_Identifiant = :identifiant1 ),(SELECT ID_droits FROM droits Where droits.ID_droits= :droit1));");
+            for ($i=0; $i < strlen($droit)-1; $i+=3) { 
+                if ($droit[$i]==0){
+                    $res = $droit[$i+1];
+                }else{
+                    $res = $droit[$i].$droit[$i+1];
+                }
+                $req4->execute(array('nom1' => $nom, 'prenom1' => $prenom, 'droit1' => $res);
+            }
+
+
                 return $req;
         }
 
@@ -35,8 +53,7 @@
                 $req2 = $db->prepare("Insert into identifiants (nom_Identifiant, mdp_Identifiant) Select :identifiant1 , :mdp1 Where not exists(select nom_Identifiant, mdp_Identifiant from identifiants where nom_Identifiant = :identifiant2 );");
                 $req2->execute(array('identifiant1' => $identifiant, 'mdp1' => $mdp, 'identifiant2' => $identifiant));
                 
-                $req3 = $db->prepare("INSERT INTO pilote (nom_Pilote, prenom_Pilote, ID_Localisation, ID_Identifiant) SELECT :nom1 , :prenom1 ,(SELECT ID_Localisation FROM localisation WHERE localisation.nom_Localisation = :ville1 ),(SELECT ID_Identifiant FROM identifiants WHERE identifiants.nom_Identifiant = :identifiant1 AND identifiants.mdp_Identifiant = :mdp1 )
-                                     WHERE NOT EXISTS (SELECT nom_Pilote, prenom_Pilote FROM pilote WHERE pilote.nom_Pilote = :nom2 AND pilote.prenom_Pilote = :prenom2 );");
+                $req3 = $db->prepare("INSERT INTO pilote (nom_Pilote, prenom_Pilote, ID_Localisation, ID_Identifiant) SELECT :nom1 , :prenom1 ,(SELECT ID_Localisation FROM localisation WHERE localisation.nom_Localisation = :ville1 ),(SELECT ID_Identifiant FROM identifiants WHERE identifiants.nom_Identifiant = :identifiant1 AND identifiants.mdp_Identifiant = :mdp1 ) WHERE NOT EXISTS (SELECT nom_Pilote, prenom_Pilote FROM pilote WHERE pilote.nom_Pilote = :nom2 AND pilote.prenom_Pilote = :prenom2 );");
                 $req3->execute(array('nom1' => $nom, 'prenom1' => $prenom, 'ville1' => $ville, 'identifiant1' => $identifiant, 'mdp1' => $mdp, 'nom2' => $nom, 'prenom2' => $prenom));
                 
 
