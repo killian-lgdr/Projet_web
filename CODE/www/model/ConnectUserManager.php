1b69e7120a17d5f08bc3bd0726ea378d3c7e9287
armanddebesse <73818074+armanddebesse@users.noreply.github.com>
2021-03-26 10:42:15 +0100

    correction erreur requete de connexion

--- CODE/www/model/ConnectUserManager.php
+++ CODE/www/model/ConnectUserManager.php
@@ -5,7 +5,7 @@
     {
         public function connectUser($pseudo){
             $db = $this->dbConnect();
-            $req = $db->prepare('SELECT nom_Identifiant, mdp_Identifiant, GROUP_CONCAT(nom_Droits SEPARATOR ',') AS droits
+            $req = $db->prepare('SELECT nom_Identifiant, mdp_Identifiant, GROUP_CONCAT(nom_Droits SEPARATOR \',\') AS droits
             FROM identifiants
             INNER JOIN attribue ON attribue.ID_Identifiant = identifiants.ID_Identifiant
             INNER JOIN droits ON droits.ID_droits = attribue.ID_droits
