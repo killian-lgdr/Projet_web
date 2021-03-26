6041a96cd61417b964f6b24f62266fd49113ce3e
armanddebesse <73818074+armanddebesse@users.noreply.github.com>
2021-03-26 09:16:13 +0100

    header remis en forme

--- CODE/www/templates_c/c96935983b58c29f6f8d9292407bd61605e63339_0.file.head.tpl.php
+++ CODE/www/templates_c/c96935983b58c29f6f8d9292407bd61605e63339_0.file.head.tpl.php
@@ -1,18 +1,18 @@
 <?php
-/* Smarty version 3.1.39, created on 2021-03-21 15:18:47
+/* Smarty version 3.1.39, created on 2021-03-25 12:13:43
   from 'D:\Ordinateur\CESI\A2\4_WEB\Projet\Projet_web\CODE\www\public\tpl\head.tpl' */
 
 /* @var Smarty_Internal_Template $_smarty_tpl */
 if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   'version' => '3.1.39',
-  'unifunc' => 'content_605755c7e32fa3_37701433',
+  'unifunc' => 'content_605c7067ef2ce4_02689170',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
     'c96935983b58c29f6f8d9292407bd61605e63339' => 
     array (
       0 => 'D:\\Ordinateur\\CESI\\A2\\4_WEB\\Projet\\Projet_web\\CODE\\www\\public\\tpl\\head.tpl',
-      1 => 1616335820,
+      1 => 1616670821,
       2 => 'file',
     ),
   ),
@@ -20,7 +20,7 @@ if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   array (
   ),
 ),false)) {
-function content_605755c7e32fa3_37701433 (Smarty_Internal_Template $_smarty_tpl) {
+function content_605c7067ef2ce4_02689170 (Smarty_Internal_Template $_smarty_tpl) {
 ?>    <head>
         <meta charset="utf-8">
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">        
--- CODE/www/templates_c/f7c6ca62334cbac7090fdb01963443660ca78cc2_0.file.header.tpl.php
+++ CODE/www/templates_c/f7c6ca62334cbac7090fdb01963443660ca78cc2_0.file.header.tpl.php
@@ -1,18 +1,18 @@
 <?php
-/* Smarty version 3.1.39, created on 2021-03-25 09:39:19
+/* Smarty version 3.1.39, created on 2021-03-25 12:03:53
   from 'D:\Ordinateur\CESI\A2\4_WEB\Projet\Projet_web\CODE\www\public\tpl\header.tpl' */
 
 /* @var Smarty_Internal_Template $_smarty_tpl */
 if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   'version' => '3.1.39',
-  'unifunc' => 'content_605c4c37ee2bf5_09923002',
+  'unifunc' => 'content_605c6e1933cf68_10046920',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
     'f7c6ca62334cbac7090fdb01963443660ca78cc2' => 
     array (
       0 => 'D:\\Ordinateur\\CESI\\A2\\4_WEB\\Projet\\Projet_web\\CODE\\www\\public\\tpl\\header.tpl',
-      1 => 1616661553,
+      1 => 1616670230,
       2 => 'file',
     ),
   ),
@@ -20,7 +20,7 @@ if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   array (
   ),
 ),false)) {
-function content_605c4c37ee2bf5_09923002 (Smarty_Internal_Template $_smarty_tpl) {
+function content_605c6e1933cf68_10046920 (Smarty_Internal_Template $_smarty_tpl) {
 ?>    <header class="row">
       <div class="col">
           <div id="navbar" class="container-fluid brd">
@@ -33,10 +33,10 @@ function content_605c4c37ee2bf5_09923002 (Smarty_Internal_Template $_smarty_tpl)
                 </div>
                 <div class="col-lg-4 col-sm-4  align-self-center"><h1 id="titre">Le Bon Stage</h1></div>
                 <div class="col-lg-3 col-sm-2 align-self-center" id="navbar-right">
+                    <a href="?action=PDView">Pilote/Délegué</a>
                     <a id="<?php echo $_smarty_tpl->tpl_vars['id']->value;?>
 " class="active" href="#h"><?php echo $_smarty_tpl->tpl_vars['con']->value;?>
 </a>
-                    <a href="?action=PDView">Pilote/Délegué</a>
                 </div>
                 <div class="col-1 align-self-center">
                     <a href=""><i id="logo" class="fas fa-sign-out-alt"></i></a>
