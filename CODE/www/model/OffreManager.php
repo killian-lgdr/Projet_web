1bc8ec691dc5bd866b965cdb15e478cc530abcf9
armanddebesse <73818074+armanddebesse@users.noreply.github.com>
2021-03-30 10:26:00 +0100

    suppression doublons id

--- CODE/www/model/OffreManager.php
+++ CODE/www/model/OffreManager.php
@@ -5,7 +5,7 @@
     {
         public function getAllOffres(){
             $db = $this->dbConnect();
-            $req = $db->query('Select offre.ID_Offre, nom_Offre, duree, salaire, date, nombreplace, nom_Localisation, nom_Entreprise, GROUP_CONCAT(`nom_Competence` SEPARATOR ", ") AS nom_Competence, promotion, offre.ID_Offre from offre inner join localisation on offre.ID_localisation=localisation.ID_localisation inner Join entreprise on offre.ID_entreprise = entreprise.ID_entreprise inner join requiert on offre.Id_offre = requiert.ID_offre inner join competence on requiert.ID_competence = competence.ID_competence inner join niveauetudes on niveauetudes.ID_NiveauEtudes = offre.ID_NiveauEtudes GROUP BY offre.ID_Offre ');
+            $req = $db->query('Select nom_Offre, duree, salaire, date, nombreplace, nom_Localisation, nom_Entreprise, GROUP_CONCAT(`nom_Competence` SEPARATOR ", ") AS nom_Competence, promotion, offre.ID_Offre from offre inner join localisation on offre.ID_localisation=localisation.ID_localisation inner Join entreprise on offre.ID_entreprise = entreprise.ID_entreprise inner join requiert on offre.Id_offre = requiert.ID_offre inner join competence on requiert.ID_competence = competence.ID_competence inner join niveauetudes on niveauetudes.ID_NiveauEtudes = offre.ID_NiveauEtudes GROUP BY offre.ID_Offre ');
             return $req;
         }
         public function getAllEntreprise(){
@@ -15,7 +15,7 @@
         }
         public function getOffre($domaine, $ville, $date, $nivetudes, $dureemin, $dureemax, $salaire, $entreprise, $depart, $OffreParPage){
             $db = $this->dbConnect();
-            $req = $db->prepare('Select offre.ID_Offre, nom_Offre, duree, salaire, date, nombrePlace, nom_Localisation, nom_Entreprise, GROUP_CONCAT(`nom_Competence` SEPARATOR ", ") AS nom_Competence, promotion, offre.ID_Offre from offre 
+            $req = $db->prepare('Select nom_Offre, duree, salaire, date, nombrePlace, nom_Localisation, nom_Entreprise, GROUP_CONCAT(`nom_Competence` SEPARATOR ", ") AS nom_Competence, promotion, offre.ID_Offre from offre 
                                 inner join localisation on offre.ID_localisation=localisation.ID_localisation 
                                 inner Join entreprise on offre.ID_entreprise = entreprise.ID_entreprise 
                                 inner join requiert on offre.Id_offre = requiert.ID_offre 
